458e99f08bf23166a7ae674701effd1d9fcd456c
250541064 <250541064@firat.edu.tr>
2025-12-03 21:50:59 +0300

    Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,7 @@
+
+
+
+
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,10 +16,11 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // Cevap: Java hata vermez. [cite_start]Metot çalışır, yan etkileri (varsa ekrana yazdırma vb.) gerçekleşir, ancak dönen değer hafızadan silinir (discarded). [cite: 246]
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
+        // Cevap: Derleyici hata (compile-time error) verir. [cite_start]Çünkü void metotlar bir değer üretmez, bu yüzden matematiksel işleme giremezler veya bir değişkene atanamazlar. [cite: 248]
 
-        // Cevaplarinizi yorum olarak ekleyin.
     }
 }
