d62ec900be4c3479600e93b9305e0448b5409e84
250541064 <250541064@firat.edu.tr>
2025-12-03 21:48:49 +0300

    Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,24 +1,33 @@
-public class Exercise4_3 {
 
-    public static void zoop() {
-        baffle();
-        System.out.print("You wugga ");
-        baffle();
-    }
+
+
+
+public class Zoop {
 
     public static void main(String[] args) {
+        // 1. Satırın başı
         System.out.print("No, I ");
         zoop();
+        
+        // 3. Satırın başı
         System.out.print("I ");
         baffle();
     }
 
+    public static void zoop() {
+        baffle(); // 1. Satırın sonunu getirir
+        
+        // 2. Satırın başı
+        System.out.print("You wugga ");
+        baffle(); // 2. Satırın sonunu getirir
+    }
+
     public static void baffle() {
         System.out.print("wug");
-        ping();
+        ping(); // Noktayı koyar ve satırı bitirir
     }
 
     public static void ping() {
-        System.out.println(".");
+        System.out.println("."); // DİKKAT: println kullanıldığı için alt satıra geçer
     }
 }
